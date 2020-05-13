77bd1254ba3de2fbc12af39dc9305a73068e16e4
Gabriel Totev <gttotev8@gmail.com>
2020-05-12 19:07:43 -0700

    Pivot to once-a-week KSM transfer

--- getdrip.sh
+++ getdrip.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 prefix="$HOME/.dripsama"
 [ ! -d $prefix ] && mkdir $prefix
 
@@ -11,6 +10,16 @@ collector_file="$prefix/collector.txt"
 at_file="$prefix/atno.txt"
 txns_file="$prefix/transfer_txns.txt"
 subkey_cmd="subkey -n kusama vanity --number 1 ksma"
+
+if which npm &> /dev/null && [ -s $collector_file -a -s $secrets_file ] \
+    && [ $(wc $secrets_file | awk '{print $1}') -gt 20 ]; then # invoke transfer.js
+    address=$(head -n 1 $collector_file)
+    pushd "${BASH_SOURCE%/*}/util"
+    [ package.json -nt node_modules ] && { rm -rf node_modules; npm install; }
+    XFER_TIMEOUT=30 ./transfer.js $address $secrets_file 2>&1 | tee -a $txns_file
+    [ $PIPESTATUS -eq 0 ] && cat $secrets_file >> "$secrets_file.old" && rm $secrets_file
+    popd
+fi
 
 for cred in $(cat $credentials_file); do
     if [ -s $pregen_file ]; then
@@ -35,13 +44,4 @@ done
 if which at &> /dev/null; then # schedule for tomorrow
     [ -s $at_file ] && atrm $(head -n 1 $at_file) &> /dev/null
     echo $BASH_SOURCE | at 'tomorrow + 1 minute' 2>&1 | tail -n 1 | awk '{print $2}' > $at_file
-fi
-
-if which npm &> /dev/null && [ -s $collector_file -a -s $secrets_file ]; then # invoke transfer.js
-    address=$(head -n 1 $collector_file)
-    pushd "${BASH_SOURCE%/*}/util"
-    [ package.json -nt node_modules ] && { rm -rf node_modules; npm install; }
-    XFER_TIMEOUT=30 ./transfer.js $address $secrets_file 2>&1 | tee -a $txns_file
-    [ $PIPESTATUS -eq 0 ] && cat $secrets_file >> "$secrets_file.old" && rm $secrets_file
-    popd
 fi
